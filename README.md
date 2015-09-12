7220b524e9ff8ec1cd21dd76968b46e25b46f5d8
Brian Lam <iBriaan@gmail.com>
2015-09-12 15:22:59 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Dynamic-Key
-PHP library to generate encryption keys that will change periodically
+# Dynamic Key
+
+Dynamic Key is a PHP library returns a randomly-generated string that can be used for an encryption key. This library will return a key that will only be valid for a certain amount of time (1 hour by default) before a new key is generated. 
+
+### Use Case
+This library was designed for encrypting data and discarding the encryption key after a certain amount of time to prevent brute-forcing.
+
+### Tech
+
+Dynamic Key was designed for PHP 5. 
+
+### Installation
+
+Download dynamic_key.php, and wherever it is needed, write:
+
+```php
+include "dynamic_key.php";
+```
+#### Author
+Brian Lam
---
+++ dynamic_key.php
@@ -0,0 +1,122 @@
+<?php
+
+class DynamicKey{
+
+	$key_file = "dynamic_key/ServerKey";
+	$date_file = "dynamic_key/KeyDate";
+	$date_format = "F j, Y, g:i a";
+	$key_life = "-1 hours";
+
+	/*
+	 *	Generate a random sequence of characters to be used for the key
+	 *	@param int $length 		length of string
+	 *	@return string $key 	randomly generated key
+	 *	
+	 */
+	function generateKeystring($length = 10) {
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$charactersLength = strlen($characters);
+		$key = '';
+		for ($i = 0; $i < $length; $i++) {
+			$key .= $characters[rand(0, $charactersLength - 1)];
+    	}
+    	return $key;
+	}
+
+	/*
+	 *	Calls method to generate key and then stores the key in specified file,
+	 *	along with a timestamp.
+	 * 	@return string $key 	randomly generated key from generateKeystring()
+	 */
+	function storeNewKey() {
+		$key = generateKeystring();
+		$time = date($this->date_format);
+		file_put_contents($key_file, $key);
+		file_put_contents($date_file, $time);
+		return $key;
+	}
+
+	/*
+	 *	Tries to find a recent key on the server. If the key doesn't exist or 
+	 *	if it is out of date, it will be replaced with a new one
+	 *
+	 *	@return string $key 	randomly generated key, or recent key
+	 */
+	function retrieveKey() {
+		if (file_exists($this->keyfile) && file_exists($this->date_file)) {
+			$date = strtotime(file_get_contents($this->date_file));
+			$key_life = strtotime($this->key_life);
+			if ($date < $key_life) {
+				return storeNewKey();
+			} else {
+				$key = strtotime(file_get_contents($this->keyfile));
+				return $key;
+			}
+		} else {
+		 	$key = storeNewKey();
+			return $key;
+		}
+	}
+
+	/*
+	 * Change the default Key file location
+	 * @param string $file_location
+	 * 
+	 *	@return string $key 	The most up-to-date key on the server
+	 */
+	function setKeyfile($file_location) {
+		if (!is_string($file_location)) {
+			throw new Exception("Invalid File Location");
+		} else {
+			$this->key_file = $file_location;
+			return retrieveKey();
+		}
+	}
+
+	/*
+	 * Change the default Date file location
+	 * @param string $file_location
+	 * 
+	 *	@return string $key 	The most up-to-date key on the server
+	 */
+	function setDatefile($file_location) {
+		if (!is_string($file_location)) {
+			throw new Exception("Invalid File Location");
+		} else {
+			$this->date_file = $file_location;
+			return retrieveKey();
+		}
+	}
+
+	/*
+	 * Change the default timestamp_format
+	 * @param string $date_format 	Example: "F j, Y, g:i a"
+	 * 
+	 *	@return string $key 	The most up-to-date key on the server
+	 */
+	function setDatefile($date_format) {
+		if (!(bool)strtotime($date_format)) {
+			throw new Exception("Invalid Date Format");
+		} else {
+			$this->date_format = $date_format;
+			return retrieveKey();
+		}
+	}
+
+	/*
+	 * Set key life
+	 * @param string $key_life	string representation of key life. A key life of
+	 							1 hour should be represented as "-1 hours"
+	 * 
+	 *	@return string $key 	The most up-to-date key on the server
+	 */
+	function setDatefile($key_life) {
+		if (!(bool)strtotime($key_life)) {
+			throw new Exception("Invalid Timestamp");
+		} else {
+			$this->key_life = $key_life;
+			return retrieveKey();
+		}
+	}
+
+}
